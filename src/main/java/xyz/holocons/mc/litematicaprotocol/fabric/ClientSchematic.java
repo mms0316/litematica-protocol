5a707e298cbb87c5c07f1b14f09df5d5c0b89115
Derek Lee <derek.scott.lee13@gmail.com>
2023-09-28 23:24:04 -0700

    Refactor schematic conversion

--- src/main/java/xyz/holocons/mc/litematicaprotocol/fabric/ClientSchematic.java
+++ src/main/java/xyz/holocons/mc/litematicaprotocol/fabric/ClientSchematic.java
@@ -1,8 +1,8 @@
 package xyz.holocons.mc.litematicaprotocol.fabric;
 
+import java.io.DataOutput;
 import java.io.IOException;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
 
 import fi.dy.masa.litematica.schematic.LitematicaSchematic;
 import fi.dy.masa.litematica.schematic.container.LitematicaBlockStateContainer;
@@ -10,14 +10,14 @@ import fi.dy.masa.litematica.schematic.placement.SchematicPlacement;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufOutputStream;
 import io.netty.buffer.Unpooled;
-import it.unimi.dsi.fastutil.doubles.DoubleList;
 import it.unimi.dsi.fastutil.objects.Object2IntArrayMap;
 import net.minecraft.block.BlockState;
-import net.minecraft.nbt.NbtByteArray;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtDouble;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtIo;
-import net.minecraft.nbt.NbtList;
+import net.minecraft.nbt.NbtType;
+import net.minecraft.nbt.scanner.NbtScanner;
+import net.minecraft.nbt.visitor.NbtElementVisitor;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.registry.Registries;
 
@@ -61,110 +61,267 @@ public class ClientSchematic {
         nbt.putShort("Width", (short) Math.abs(size.getX()));
         nbt.putShort("Height", (short) Math.abs(size.getY()));
         nbt.putShort("Length", (short) Math.abs(size.getZ()));
-        final var container = new BlockContainer(schematic.getSubRegionContainer(regionName));
-        nbt.put("Palette", container.getPalette());
-        nbt.put("BlockData", container.getBlockData());
-        final var blockEntities = getBlockEntities(schematic, regionName);
+        final var container = schematic.getSubRegionContainer(regionName);
+        final var palette = new Palette(container);
+        nbt.put("Palette", palette);
+        final var blockData = new BlockData(palette, container);
+        nbt.put("BlockData", blockData);
+        final var blockEntities = new BlockEntities(schematic, regionName);
         if (!blockEntities.isEmpty()) {
             nbt.put("BlockEntities", blockEntities);
         }
-        final var entities = getEntities(schematic, regionName);
+        final var entities = new Entities(schematic, regionName);
         if (!entities.isEmpty()) {
             nbt.put("Entities", entities);
         }
         return nbt;
     }
 
-    private static final class BlockContainer {
+    private interface NbtSchematicElement extends NbtElement {
 
-        private final LitematicaBlockStateContainer blocks;
-        private final Object2IntArrayMap<BlockState> blockStates;
+        static void write(final String key, final NbtElement element, final DataOutput output) throws IOException {
+            final var type = element.getType();
+            output.writeByte(type);
+            if (type != NbtElement.END_TYPE) {
+                output.writeUTF(key);
+                element.write(output);
+            }
+        }
 
-        public BlockContainer(final LitematicaBlockStateContainer container) {
-            this.blocks = container;
+        @Override
+        default NbtType<?> getNbtType() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        default NbtElement copy() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        default int getSizeInBytes() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        default void accept(NbtElementVisitor visitor) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        default NbtScanner.Result doAccept(NbtScanner visitor) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private static final class Palette implements NbtSchematicElement {
+
+        private final Object2IntArrayMap<BlockState> states;
+
+        public Palette(final LitematicaBlockStateContainer container) {
             final var palette = container.getPalette();
-            this.blockStates = new Object2IntArrayMap<>(palette.getPaletteSize());
-            blockStates.defaultReturnValue(-1);
+            this.states = new Object2IntArrayMap<>(palette.getPaletteSize());
+            states.defaultReturnValue(-1);
             for (int i = 0; i < palette.getPaletteSize(); i++) {
-                final var state = palette.getBlockState(i);
-                if (!blockStates.containsKey(state)) {
-                    final var id = blockStates.size();
-                    blockStates.put(state, id);
+                final var nullableState = palette.getBlockState(i);
+                final var state = nullableState != null ? nullableState : LitematicaBlockStateContainer.AIR_BLOCK_STATE;
+                if (!states.containsKey(state)) {
+                    final var id = states.size();
+                    states.put(state, id);
                 }
             }
         }
 
-        public NbtCompound getPalette() {
-            final var nbt = new NbtCompound();
-            blockStates.forEach((state, id) -> nbt.putInt(formatBlockState(state), id));
-            return nbt;
+        public int getId(final BlockState state) {
+            return states.getInt(state);
+        }
+
+        public int size() {
+            return states.size();
+        }
+
+        @Override
+        public void write(DataOutput output) throws IOException {
+            for (final var entry : states.object2IntEntrySet()) {
+                final var state = entry.getKey();
+                final var id = entry.getIntValue();
+                output.writeByte(NbtElement.INT_TYPE);
+                output.writeUTF(formatBlockState(state));
+                output.writeInt(id);
+            }
+            output.writeByte(NbtElement.END_TYPE);
         }
 
         private static String formatBlockState(final BlockState state) {
-            final var nonNullBlockState = state != null ? state : LitematicaBlockStateContainer.AIR_BLOCK_STATE;
             final var propertyJoiner = new StringJoiner(",", "[", "]").setEmptyValue("");
-            nonNullBlockState.getEntries().forEach((property, comparable) -> propertyJoiner
+            state.getEntries().forEach((property, comparable) -> propertyJoiner
                     .add(property.getName() + '=' + String.valueOf(comparable).toLowerCase()));
-            return Registries.BLOCK.getId(nonNullBlockState.getBlock()).toString() + propertyJoiner.toString();
+            return Registries.BLOCK.getId(state.getBlock()).toString() + propertyJoiner.toString();
         }
 
-        public NbtByteArray getBlockData() {
-            final var sizeX = blocks.getSize().getX();
-            final var sizeY = blocks.getSize().getY();
-            final var sizeZ = blocks.getSize().getZ();
-            final var maxVarIntLength = PacketByteBuf.getVarIntLength(blockStates.size() - 1);
+        @Override
+        public byte getType() {
+            return NbtElement.COMPOUND_TYPE;
+        }
+    }
+
+    private static final class BlockData implements NbtSchematicElement {
+
+        private final PacketByteBuf data;
+
+        public BlockData(final Palette palette, final LitematicaBlockStateContainer container) {
+            final var sizeX = container.getSize().getX();
+            final var sizeY = container.getSize().getY();
+            final var sizeZ = container.getSize().getZ();
+            final var maxVarIntLength = PacketByteBuf.getVarIntLength(palette.size() - 1);
             final var bufferSize = sizeX * sizeY * sizeZ * maxVarIntLength;
-            final var data = new PacketByteBuf(Unpooled.buffer(bufferSize, bufferSize));
+            this.data = new PacketByteBuf(Unpooled.buffer(bufferSize, bufferSize));
             for (int y = 0; y < sizeY; y++) {
                 for (int z = 0; z < sizeZ; z++) {
                     for (int x = 0; x < sizeX; x++) {
-                        data.writeVarInt(blockStates.getInt(blocks.get(x, y, z)));
+                        data.writeVarInt(palette.getId(container.get(x, y, z)));
                     }
                 }
             }
-            return new NbtByteArray(data.getWrittenBytes());
+        }
+
+        @Override
+        public void write(DataOutput output) throws IOException {
+            output.writeInt(data.writerIndex());
+            output.write(data.array(), 0, data.writerIndex());
+        }
+
+        @Override
+        public byte getType() {
+            return NbtElement.BYTE_ARRAY_TYPE;
         }
     }
 
-    private static NbtList getBlockEntities(final LitematicaSchematic schematic, final String regionName) {
-        return schematic.getBlockEntityMapForRegion(regionName).values().stream()
-                .map(blockEntity -> {
-                    final var nbt = blockEntity.copy();
-                    final var position = new int[] {
-                            nbt.getInt("x"),
-                            nbt.getInt("y"),
-                            nbt.getInt("z")
-                    };
-                    final var id = nbt.getString("id");
-                    nbt.remove("x");
-                    nbt.remove("y");
-                    nbt.remove("z");
-                    nbt.remove("id");
-                    nbt.putIntArray("Pos", position);
-                    nbt.putString("Id", id);
-                    return nbt;
-                })
-                .collect(Collectors.toCollection(NbtList::new));
+    private static final class BlockEntities implements NbtSchematicElement {
+
+        private final LitematicaSchematic schematic;
+        private final String regionName;
+
+        public BlockEntities(final LitematicaSchematic schematic, final String regionName) {
+            this.schematic = schematic;
+            this.regionName = regionName;
+        }
+
+        public boolean isEmpty() {
+            return schematic.getBlockEntityMapForRegion(regionName).isEmpty();
+        }
+
+        @Override
+        public void write(DataOutput output) throws IOException {
+            final var blockEntites = schematic.getBlockEntityMapForRegion(regionName);
+            final var container = schematic.getSubRegionContainer(regionName);
+            output.writeByte(NbtElement.COMPOUND_TYPE);
+            output.writeInt(blockEntites.size());
+            for (final var entry : blockEntites.entrySet()) {
+                final var position = entry.getKey();
+                final var nbt = entry.getValue();
+                final var x = position.getX();
+                final var y = position.getY();
+                final var z = position.getZ();
+                writePos(x, y, z, output);
+                writeId(container.get(x, y, z), output);
+                writeExtra(nbt, output);
+                output.writeByte(NbtElement.END_TYPE);
+            }
+        }
+
+        private static void writePos(final int x, final int y, final int z, final DataOutput output)
+                throws IOException {
+            output.writeByte(NbtElement.INT_ARRAY_TYPE);
+            output.writeUTF("Pos");
+            output.writeInt(3);
+            output.writeInt(x);
+            output.writeInt(y);
+            output.writeInt(z);
+        }
+
+        private static void writeId(final BlockState state, final DataOutput output) throws IOException {
+            output.writeByte(NbtElement.STRING_TYPE);
+            output.writeUTF("Id");
+            output.writeUTF(Registries.BLOCK.getId(state.getBlock()).toString());
+        }
+
+        private static void writeExtra(final NbtCompound nbt, final DataOutput output) throws IOException {
+            for (final var key : nbt.getKeys()) {
+                switch (key) {
+                    case "id", "x", "y", "z" -> {
+                    }
+                    default -> NbtSchematicElement.write(key, nbt.get(key), output);
+                }
+            }
+        }
+
+        @Override
+        public byte getType() {
+            return NbtElement.LIST_TYPE;
+        }
     }
 
-    private static NbtList getEntities(final LitematicaSchematic schematic, final String regionName) {
-        final var positionOffset = schematic.getSubRegionPosition(regionName);
-        return schematic.getEntityListForRegion(regionName).stream()
-                .map(entity -> {
-                    final var nbt = entity.nbt.copy();
-                    final var position = DoubleList.of(
-                            entity.posVec.x + positionOffset.getX(),
-                            entity.posVec.y + positionOffset.getY(),
-                            entity.posVec.z + positionOffset.getZ())
-                            .doubleStream()
-                            .mapToObj(NbtDouble::of)
-                            .collect(Collectors.toCollection(NbtList::new));
-                    final var id = nbt.getString("id");
-                    nbt.remove("id");
-                    nbt.put("Pos", position);
-                    nbt.putString("Id", id);
-                    return nbt;
-                })
-                .collect(Collectors.toCollection(NbtList::new));
+    private static final class Entities implements NbtSchematicElement {
+
+        private final LitematicaSchematic schematic;
+        private final String regionName;
+
+        public Entities(final LitematicaSchematic schematic, final String regionName) {
+            this.schematic = schematic;
+            this.regionName = regionName;
+        }
+
+        public boolean isEmpty() {
+            return schematic.getEntityListForRegion(regionName).isEmpty();
+        }
+
+        @Override
+        public void write(DataOutput output) throws IOException {
+            final var entities = schematic.getEntityListForRegion(regionName);
+            final var regionPosition = schematic.getSubRegionPosition(regionName);
+            output.writeByte(NbtElement.COMPOUND_TYPE);
+            output.writeInt(entities.size());
+            for (final var entity : entities) {
+                final var nbt = entity.nbt;
+                final var x = (double) regionPosition.getX() + entity.posVec.x;
+                final var y = (double) regionPosition.getY() + entity.posVec.y;
+                final var z = (double) regionPosition.getZ() + entity.posVec.z;
+                writePos(x, y, z, output);
+                writeId(nbt.get("id"), output);
+                writeExtra(nbt, output);
+                output.writeByte(NbtElement.END_TYPE);
+            }
+        }
+
+        private static void writePos(final double x, final double y, final double z, final DataOutput output)
+                throws IOException {
+            output.writeByte(NbtElement.LIST_TYPE);
+            output.writeUTF("Pos");
+            output.writeByte(NbtElement.DOUBLE_TYPE);
+            output.writeInt(3);
+            output.writeDouble(x);
+            output.writeDouble(y);
+            output.writeDouble(z);
+        }
+
+        private static void writeId(final NbtElement id, final DataOutput output) throws IOException {
+            NbtSchematicElement.write("Id", id, output);
+        }
+
+        private static void writeExtra(final NbtCompound nbt, final DataOutput output) throws IOException {
+            for (final var key : nbt.getKeys()) {
+                switch (key) {
+                    case "id", "Pos" -> {
+                    }
+                    default -> NbtSchematicElement.write(key, nbt.get(key), output);
+                }
+            }
+        }
+
+        @Override
+        public byte getType() {
+            return NbtElement.LIST_TYPE;
+        }
     }
 }
